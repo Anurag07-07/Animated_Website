912646767002520df0622e5ee932b0af0e70db5e
Anurag07-07 <adityanv8@gmail.com>
2026-01-17 22:59:41 +0530

    some changes

--- src/Components/Home/HomeBottomText.tsx
+++ src/Components/Home/HomeBottomText.tsx
@@ -4,7 +4,7 @@ const HomeBottomText = () => {
   return (
     <div className=' font-[font1] flex justify-center items-center gap-2 '>
       <Link to={'/projects'} className=" text-[6.5vw] leading-[6vw]  border-3 px-6 pt-3 uppercase pb-0   rounded-full "  >Projects</Link>
-      <Link to={'/agencies'} className=" text-[6.5vw] leading-[6vw]  border-3 px-6 pt-3 uppercase pb-0   rounded-full "  >Agency</Link>
+      <Link to={'/agence'} className=" text-[6.5vw] leading-[6vw]  border-3 px-6 pt-3 uppercase pb-0   rounded-full "  >Agency</Link>
     </div>
   )
 }
