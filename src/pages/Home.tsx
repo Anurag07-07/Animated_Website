d95c942ba9c6a0500ff923d6caf89cc599559b90
Anurag07-07 <adityanv8@gmail.com>
2026-01-17 12:25:40 +0530

    added video on home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Video from '../Components/Home/Video'
+
+const Home = () => {
+  return (
+    <div className=' h-screen w-screen bg-red-900'>
+      <Video></Video>
+    </div>
+  )
+}
+
+export default Home
