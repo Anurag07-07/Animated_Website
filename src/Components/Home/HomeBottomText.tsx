71380ce8fa48b1041c745b3c445bac3869b4ce65
Anurag07-07 <adityanv8@gmail.com>
2026-01-18 14:41:11 +0530

    Main page agency writing work completed

--- src/Components/Home/HomeBottomText.tsx
+++ src/Components/Home/HomeBottomText.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 const HomeBottomText = () => {
   return (
     <div className=' font-[font1] flex justify-center items-center gap-2 '>
-      <Link to={'/projects'} className=" text-[6.5vw] leading-[6vw]  border-3 px-6 pt-3 uppercase pb-0   rounded-full "  >Projects</Link>
-      <Link to={'/agence'} className=" text-[6.5vw] leading-[6vw]  border-3 px-6 pt-3 uppercase pb-0   rounded-full "  >Agency</Link>
+      <Link to={'/projects'} className=" text-[6.5vw] leading-[6vw]  border-3 px-6 pt-3 uppercase pb-0   rounded-full  hover:border-[#D3FD50]  hover:text-[#D3FD50]    "  >Projects</Link>
+      <Link to={'/agence'} className=" text-[6.5vw] leading-[6vw]  border-3 px-6 pt-3 uppercase pb-0   rounded-full    hover:border-[#D3FD50] hover:text-[#D3FD50]    "  >Agency</Link>
     </div>
   )
 }
--- src/pages/Agence.tsx
+++ src/pages/Agence.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
 const Agence = () => {
   return (
-    <div>Agence</div>
+    <div>
+      <div className=" absolute h-86 w-56   overflow-hidden rounded-4xl top-[20vh] left-[31vw]">
+        <img alt="image" className=" h-full w-full object-cover" src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7"></img>
+      </div>
+    <div className=" font-[font2] relative ">
+      <div className=" mt-[52vh]">
+        <h1 className=" text-center text-[19vw] leading-[18vw] uppercase">SEVEN7Y<br></br>TWO</h1>
+      </div>
+      <div className="  pl-[45vw]">
+        <p className=" text-6xl"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+      </div>
+    </div>
+    </div>
   )
 }
 
