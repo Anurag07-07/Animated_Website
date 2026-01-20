8a7078e4ef31cce2c963ed265be7279ed49deb8b
Anurag07-07 <adityanv8@gmail.com>
2026-01-20 13:32:04 +0530

    added transition stair animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,42 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Agence from "./pages/Agence"
 import Projects from "./pages/Projects"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+
+
 
 const App = () => {
+  
+  useGSAP(()=>{
+
+    const tl = gsap.timeline()
+    
+    tl.from('.stairs ',{
+      height:0,
+      stagger:{
+        amount:-0.3
+      }
+    })
+
+    tl.to('.stairs ',{
+      y:"100%",
+      stagger:{
+        amount:-0.3
+      }     
+    })
+
+  })
+
   return (
     <div className=" text-6xl text-white">
+      <div className=" w-full h-full fixed z-20 top-0 flex">
+        <div className=" stairs  w-1/5 h-screen bg-black"></div>
+        <div className=" stairs  w-1/5 h-screen bg-black"></div>
+        <div className=" stairs  w-1/5 h-screen bg-black"></div>
+        <div className=" stairs  w-1/5 h-screen bg-black"></div>
+        <div className=" stairs  w-1/5 h-screen bg-black"></div>
+      </div>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/agence" element={<Agence></Agence>}></Route>
